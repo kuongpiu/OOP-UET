a78f54ea541c7b130372d3b7ffd254c602f966fc
cuongnong <cuongcuongnong@gmail.com>
2019-11-05 00:07:33 +0700

    init

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+void chuanHoa(string &str){
+    int fi = 0;
+    int la = 0;
+    int length = str.length();
+    while(str[fi] == ' '){
+        fi++;
+    }
+    while(str[length - 1 - la] == ' '){
+        la++;
+    }
+    la = length - la - fi;
+    str = str.substr(fi, la);
+
+    str = str + " ";
+    int lo = 0;
+    int hi = str.find("www ", lo);
+    cout << hi << endl;
+}
 int main(){
-    cout << "thiss is mine";
+    string str;
+    getline(cin,str);
+    chuanHoa(str);
+    cout << "." + str + ".";
     return 0;
 }
